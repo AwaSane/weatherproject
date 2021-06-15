a3c758ef02fae073fba573b9216a87a21beb0c06
Awa Sane <awasane.943@gmail.com>
2021-06-15 16:24:17 +0200

    mise en place de l'API Openweathermap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import VueGooglePlaces from 'vue-google-places'
 
 Vue.config.productionTip = false
-
+// Vue.use(VueGooglePlaces)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
